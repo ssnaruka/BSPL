861605cbb9d8c23e6d6bae2fdfa3beae3289d94f
SURENDRA SINGH <ssnaruka@users.noreply.github.com>
2023-01-27 12:28:55 +0530

    -add splash screen functionality

--- app/src/main/java/com/example/bspl/MainActivity.kt
+++ app/src/main/java/com/example/bspl/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.bspl
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.bspl.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val mViewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            // hold splash screen
+            setKeepOnScreenCondition {
+                mViewModel.isLoading.value
+            }
+        }
         setContentView(R.layout.activity_main)
     }
 }
